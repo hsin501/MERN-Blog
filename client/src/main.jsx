196998bf5f5dfb01378a30e109b5f9703854db0c
hsin <joanne45644@gmail.com>
2024-03-12 23:06:06 +0800

    add redux Persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { store } from './redux/store.js';
+import { store, persistor } from './redux/store.js';
+// redux toolkit
 import { Provider } from 'react-redux';
+//用於加載持久化存儲的組件，它確保在重播儲存的狀態之前，將儲存的狀態加載到 Redux store 中
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
