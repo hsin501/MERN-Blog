901d0fe02813be6ae8900d0cddcedc2e1a76ed3f
hsin <joanne45644@gmail.com>
2024-03-15 18:07:56 +0800

    add sidebar of the dashboard

---
+++ client/src/components/DashboardSidebar.jsx
@@ -0,0 +1,42 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { Sidebar } from 'flowbite-react';
+import { HiUser, HiArrowSmRight } from 'react-icons/hi';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+export default function DashboardSidebar() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    // console.log(tabFromUrl);
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <Sidebar className='w-full md:w-56'>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to='/dashboard?tab=profile'>
+            <Sidebar.Item
+              active={tab === 'profile'}
+              icon={HiUser}
+              label={'User'}
+              labelColor='dark'
+              as='div'
+            >
+              個人資料
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item icon={HiArrowSmRight} labelColor='dark'>
+            登出
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
