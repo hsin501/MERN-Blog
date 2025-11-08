6b13cd91d08327dfb7fbb11e9820b9fb14157dc2
hsin <joanne45644@gmail.com>
2025-11-09 01:04:54 +0800

    1.add:上船相關設定

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
+import path from 'path';
 
 dotenv.config(); // 載入環境變數
 
@@ -18,6 +19,8 @@ mongoose // 連接到 MongoDB
     console.log('MongoDB Error', err);
   });
 
+const __dirname = path.resolve(); // 取得當前目錄路徑
+
 const app = express(); // 建立 Express 應用程式
 const port = 3000; // 設定應用程式埠號
 app.use(express.json()); // 設置 Express 使用 JSON 格式的請求
@@ -28,6 +31,12 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist'))); // 提供靜態檔案服務
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+}); // 處理所有未匹配的路由，返回前端應用程式的 index.html
 
 app.listen(port, () => {
   console.log(` listening on port ${port}`);
